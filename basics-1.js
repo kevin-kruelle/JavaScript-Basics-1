f0c607ed4b8f4d901762a35af83914c10c54b3c6
dnkru <50063920+dnkru@users.noreply.github.com>
2021-01-28 15:04:17 -0500

    Add 4 colors to the console log

--- basics-1.js
+++ basics-1.js
@@ -7,3 +7,11 @@ alert('This is my tedtalk');
 alert('I\'m not sure what else to say');
 
 alert('This is my last alert');
+
+console.log ('Grey');
+
+console.log ('Black');
+
+console.log ('Red');
+
+console.log ('Blue');
