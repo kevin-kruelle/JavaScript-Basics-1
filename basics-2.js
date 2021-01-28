ab0cf8f3ca3b0426549cc8cf49c56e5536ccda78
dnkru <50063920+dnkru@users.noreply.github.com>
2021-01-28 15:53:28 -0500

    added values for the console.log

--- basics-2.js
+++ basics-2.js
@@ -1,1 +1,13 @@
+console.log('basics 2');
+
 const piValue = alert(Math.PI);
+
+console.log(1 + 1);
+
+console.log(Math.PI * 2);
+
+console.log(10 / 0);
+
+console.log(365 * 24);
+
+console.log(365 * 24 * 60);
